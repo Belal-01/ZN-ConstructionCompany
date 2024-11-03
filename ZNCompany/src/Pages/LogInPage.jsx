8debc776a9b07334c98320458b84b933e22ceffe
Belal-01 <belalkhobieh343@gmail.com>
2024-11-03 22:23:35 +0300

    change the login img backgroun smouthly

--- ZNCompany/src/Pages/LogInPage.jsx
+++ ZNCompany/src/Pages/LogInPage.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import LogInForm from '../Sections/LogInForm'
 import Navbar from '../Sections/Navbar'
 import {  useLocation } from 'react-router-dom'
@@ -7,6 +7,24 @@ import {  useLocation } from 'react-router-dom'
 const LogInPage = () => {
 const location = useLocation()
 const routPath = location.state?.path||'/profilePage'
+const [logInImg,setLogInImg] = useState("/imgs/logIn-1.png")
+
+useEffect(()=>{
+  setInterval(()=>{
+    const randomeNum = Math.random()
+    if(randomeNum<=2/6)
+      setLogInImg("/imgs/logIn-2.png")
+    else if(randomeNum<=3/6)
+      setLogInImg("/imgs/logIn-3.png")
+    else if(randomeNum<=4/6)
+      setLogInImg("/imgs/logIn-4.png")
+    else if(randomeNum<=5/6)
+      setLogInImg("/imgs/logIn-5.png")
+    else if(randomeNum<=6/6)
+      setLogInImg("/imgs/logIn-6.png")
+
+  },5000)
+},[])
 
   return (
     <>
@@ -18,7 +36,7 @@ const routPath = location.state?.path||'/profilePage'
             <LogInForm routPath={routPath}/>
         </div>
         <div className="login-img md:flex-1 max-md:h-[300px] w-full min-h-full max-md:relative overflow-hidden">
-          <img src="/imgs/logIn-1.png" alt="LoginImg" className='w-full min-h-full max-md:h-[350px] max-md:absolute bottom-0 z-10' />
+          <img src={logInImg} alt="LoginImg" className='w-full min-h-full max-md:h-[350px] max-md:absolute bottom-0 z-10' />
         </div>
       </div>
     </>
--- ZNCompany/src/Pages/SignUpPage.jsx
+++ ZNCompany/src/Pages/SignUpPage.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Outlet } from 'react-router'
 import Navbar from '../Sections/Navbar'
 
 const SignUpPage = () => {
+  const [signUpImg,setLogInImg] = useState("/imgs/logIn-1.png")
+
+useEffect(()=>{
+  setInterval(()=>{
+    const randomeNum = Math.random()
+    if(randomeNum<=2/6)
+      setLogInImg("/imgs/logIn-2.png")
+    else if(randomeNum<=3/6)
+      setLogInImg("/imgs/logIn-3.png")
+    else if(randomeNum<=4/6)
+      setLogInImg("/imgs/logIn-4.png")
+    else if(randomeNum<=5/6)
+      setLogInImg("/imgs/logIn-5.png")
+    else if(randomeNum<=6/6)
+      setLogInImg("/imgs/logIn-6.png")
+
+  },5000)
+},[])
   return (
     <>
     <Navbar />
     <div className="flex flex-row max-md:flex-col w-full min-h-full max-md:h-fit items-stretch max-md:items-center">
       <div className="login-img flex-1 relative max-md:h-[300px] w-full ">
-        <img src="/imgs/SingUp-1.png" alt="LoginImg" className='w-full h-full max-md:h-[300px]' loading='lazy'/>
+        <img src={signUpImg} alt="LoginImg" className='w-full h-full max-md:h-[300px]' loading='lazy'/>
         <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center ">
           <div className="flex flex-row justify-center w-[350px] h-[350px] rounded-full bg-zn-gray-box text-[64px]  max-md:flex-col max-md:w-[250px] max-md:h-[250px]  max-md:bg-zn-gray-box  max-md:top-[10%] max-md:left-[30%] max-sm:left-[20%]  z-10 ">
             <div className="flex flex-col justify-center gap-y-6">
